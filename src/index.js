d05b4c0f2c443724f37d668a624ee35142d15ac6
T-tsuchiya-lei <t.tsuchiya@leihauoli.com>
2020-11-19 18:43:22 +0900

    installed redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import MainContainer from './containers/MainContainer'
 import reducer from './reducers/reducer'
 
-const store = createStore(reducer, {})
+const store = createStore(reducer, {});
 
 ReactDOM.render(
   <Provider store={store}>
